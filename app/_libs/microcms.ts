897ef9678b82d14fcf44c557776f0c047cf03124
Seiji <fujisanroku53@outlook.jp>
2024-08-24 16:09:37 +0900

    下書きプレビュー時はSSRをする

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -4,6 +4,7 @@ import type {
   MicroCMSImage,
   MicroCMSListContent,
 } from "microcms-js-sdk";
+import { revalidate } from "../news/page";
 
 export type Member = {
   name: string;
@@ -61,6 +62,11 @@ export const getNewsDetail = async (
     endpoint: "news",
     contentId,
     queries,
+    customRequestInit: {
+      next: {
+        revalidate: queries?.draftKey === undefined ? 60 : 0,
+      },
+    },
   });
   return detailData;
 };
